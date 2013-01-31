81404a799ceddbe160fa67b14cbce250c3de74d6
Thanhnd <thanhnd.ltt@gmail.com>
2013-01-31 14:18:24 +0700

    Lấy lại post entity

--- Thanhnd/JPTRAVEL/JPTRAVEL/Classes/entity/PostEntity.m
+++ Thanhnd/JPTRAVEL/JPTRAVEL/Classes/entity/PostEntity.m
@@ -18,14 +18,32 @@
     if (self) {
         
         if(dict){
-            self.postId = [dict valueForKeyPath:@"id"];
-            self.description = [dict objectForKey:@"description"];
-            self.placeName = [dict objectForKey:@"place_name"];
-            self.postedAt = [dict objectForKey:@"posted_at"];
-            self.postThumbUrl = [dict objectForKey:@"post_thumb_url"];
-            self.postUrl = [dict objectForKey:@"post_url"];
-            self.commentCount = [[dict objectForKey:@"comment_count"] integerValue];
-            self.bookmarkCount = [[dict objectForKey:@"bookmark_count"] integerValue];  
+            
+            if([dict valueForKey:@"id"] != (id)[NSNull null]){
+                self.postId = [dict objectForKey:@"id"];
+            }
+            if([dict valueForKey:@"description"] != (id)[NSNull null] && [[dict valueForKey:@"description"] length] != 0){
+                self.description = [dict objectForKey:@"description"];
+            }
+            if([dict valueForKey:@"place_name"] != (id)[NSNull null] && [[dict valueForKey:@"place_name"] length] != 0){
+                self.placeName = [dict objectForKey:@"place_name"];
+            }
+            if([dict valueForKey:@"posted_at"] != (id)[NSNull null] && [[dict valueForKey:@"posted_at"] length] != 0){
+                self.postedAt = [dict objectForKey:@"posted_at"];
+            }
+            if([dict valueForKey:@"post_thumb_url"] != (id)[NSNull null] && [[dict valueForKey:@"post_thumb_url"] length] != 0){
+                self.postThumbUrl = [dict objectForKey:@"post_thumb_url"];
+            }
+            if([dict valueForKey:@"post_url"] != (id)[NSNull null] && [[dict valueForKey:@"post_url"] length] != 0){
+                self.postUrl = [dict objectForKey:@"post_url"];
+            }
+            
+            if([dict valueForKey:@"comment_count"] != (id)[NSNull null]){
+                self.commentCount = [[dict objectForKey:@"comment_count"] integerValue];
+            }
+            if([dict valueForKey:@"bookmark_count"] != (id)[NSNull null]){
+                self.bookmarkCount = [[dict objectForKey:@"bookmark_count"] integerValue];
+            }     
         }
     }
     
